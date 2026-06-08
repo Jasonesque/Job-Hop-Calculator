41650339528abd8e3ddf5b8a9b021dc4aa86c6f2
Jasonesque <jasonliu1922011@gmail.com>
2026-06-08 18:29:44 +0800

    chore: configure base path and GitHub Actions for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Job-Hop-Calculator/", // Set base path for GitHub Pages
   plugins: [react()],
+  server: {
+    host: true, // Listen on all network addresses, including LAN and public
+    port: 5173,
+  }
 })
